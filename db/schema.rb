10ab671423b3e5d695af679fc6d2fb702f6cdb79
Haytham Hamdy <haytham24493@gmail.com>
2018-11-11 00:49:46 +0000

    destory user table

--- db/schema.rb
+++ db/schema.rb
@@ -20,11 +20,6 @@ ActiveRecord::Schema.define(version: 20181110210238) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+  
 
 end
