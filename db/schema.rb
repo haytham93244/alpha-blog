8dc5fdadd75d7a27f8fc43c8099572612d0c467f
Haytham Hamdy <haytham24493@gmail.com>
2018-11-11 00:40:50 +0000

    create validtion for user

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20181110210238) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
