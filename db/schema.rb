64f4d5125eb66131fd581f4698f6da87ce299d75
Haytham Hamdy <haytham24493@gmail.com>
2018-11-12 21:45:17 +0000

    create user table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181110210238) do
+ActiveRecord::Schema.define(version: 20181112212829) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20181110210238) do
     t.datetime "updated_at"
   end
 
-  
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+  end
 
 end
