25dcecb9c7a85afe54f7b7832ee36dc7d1ce7010
Haytham Hamdy <haytham24493@gmail.com>
2018-11-13 00:05:06 +0000

    add secure password and attribute to users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,7 @@ before_action :set_article ,only: [:edit ,:update, :show, :destroy]
   
   def create 
     @article = Article.new(article_params)
+    @article.user= User.first
     # @article.save
     # redirect_to article_path(@article)
     
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181112224821) do
+ActiveRecord::Schema.define(version: 20181112235322) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -22,8 +22,11 @@ ActiveRecord::Schema.define(version: 20181112224821) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
