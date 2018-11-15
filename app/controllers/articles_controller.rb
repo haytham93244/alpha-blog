4e568ef33177e0fd595c83c1923cf4a29f1cd54b
Haytham Hamdy <haytham24493@gmail.com>
2018-11-15 18:04:32 +0000

    Add admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
     before_action :set_article ,only: [:edit ,:update, :show, :destroy]
-    #before_action :require_user ,except: [:index ,:show]
+    before_action :require_user ,except: [:index ,:show]
     before_action :require_same_user ,only: [:edit, :update, :destroy]
 
   def index
@@ -61,7 +61,7 @@ class ArticlesController < ApplicationController
   end
   
   def require_same_user 
-    if current_user != @article.user
+    if current_user != @article.user and !current_user.admin?
       flash[:danger] = "You can only edit or destroy your own articles"  
       redirect_to root_path
       
